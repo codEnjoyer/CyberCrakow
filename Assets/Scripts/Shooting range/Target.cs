76ccf06e5ff8d3cb48bcb4dbfa3998a4d48b49fd
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-05-22 12:35:04 +0500

    Начал реализацию мишеней

---
+++ Assets/Scripts/Shooting range/Target.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Shooting_range
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Collider))]
+    public class Target : MonoBehaviour
+    {
+        public UnityEvent OnHit { get; private set; }
+        private Rigidbody _rigidbody;
+        private Animator _targetAnimator;
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody.isKinematic = true;
+
+            _targetAnimator = GetComponentInParent<Animator>();
+
+            OnHit = new UnityEvent();
+            OnHit.AddListener(GetHit);
+        }
+
+        public void GetHit()
+        {
+            _targetAnimator.Play("TargetHitted");
+            Debug.Log("I got hit!");
+        }
+        
+        private void OnCollisionEnter(Collision other)
+        {
+            if (!other.gameObject.TryGetComponent<Bullet>(out var bullet)) return;
+            Debug.Log("Collided");
+            OnHit?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Shooting range/TargetManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooting_range
+{
+    public class TargetManager : MonoBehaviour
+    {
+        [SerializeField] private List<Target> _targets;
+
+        private void Start()
+        {
+            StartCoroutine(HittingAllTargets());
+        }
+
+
+        private IEnumerator HittingAllTargets()
+        {
+            yield return new WaitForSeconds(3);
+            foreach (var target in _targets)
+            {
+                target.GetHit();
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+}
