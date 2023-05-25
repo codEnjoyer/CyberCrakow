0684b3a31c882d6698766a2047c239a688f3d409
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-05-25 17:29:45 +0500

    Анимации попадания и восстановления, настраиваемый target manager

---
+++ Assets/Scripts/Shooting range/ITarget.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Events;
+
+namespace Shooting_range
+{
+    public interface ITarget
+    {
+        public UnityEvent OnHit { get; }
+        public void GetHit();
+        public void Recover();
+    }
+}
---
+++ Assets/Scripts/Shooting range/PolygonTargetManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooting_range
+{
+    public class PolygonTargetManager : MonoBehaviour
+    {
+        [SerializeField] private List<PolygonTarget> _targets;
+        [SerializeField] private bool _hitAllTargetsAtStart = true;
+        [SerializeField] private float _secondsToWaitBeforeHit = 3f;
+        [SerializeField] private float _secondsBetweenHits = 1f;
+        [SerializeField] private float _secondsToWaitBeforeRecover = 5f;
+        [SerializeField] private float _secondsBetweenRecovers = .5f;
+        private void Start()
+        {
+            if (_hitAllTargetsAtStart)
+                StartCoroutine(HitAllTargets());
+        }
+
+
+        private IEnumerator HitAllTargets()
+        {
+            yield return new WaitForSeconds(_secondsToWaitBeforeHit);
+            foreach (var target in _targets)
+            {
+                target.GetHit();
+                yield return new WaitForSeconds(_secondsBetweenHits);
+            }
+            StartCoroutine(RecoverAllTargets());
+        }
+        
+        private IEnumerator RecoverAllTargets()
+        {
+            yield return new WaitForSeconds(_secondsToWaitBeforeRecover);
+            foreach (var target in _targets)
+            {
+                target.Recover();
+                yield return new WaitForSeconds(_secondsBetweenRecovers);
+            }
+        }
+    }
+}
