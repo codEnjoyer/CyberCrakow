097ab82e2adfaca74c28165afdee8461ba4df61f
codEnjoyer <ch4shkin.nikita@gmail.com>
2023-05-25 17:29:45 +0500

    Анимации попадания и восстановления, настраиваемый target manager

---
+++ Assets/Scripts/Shooting range/AbstractTarget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Shooting_range
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(Collider))]
+    public abstract class AbstractTarget : MonoBehaviour, ITarget
+    {
+        public abstract UnityEvent OnHit { get; protected set; }
+
+        public virtual void GetHit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual void Recover()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- Assets/Scripts/Shooting range/PolygonTarget.cs
+++ Assets/Scripts/Shooting range/PolygonTarget.cs
@@ -3,13 +3,14 @@ using UnityEngine.Events;
 
 namespace Shooting_range
 {
-    [RequireComponent(typeof(Rigidbody))]
-    [RequireComponent(typeof(Collider))]
-    public class Target : MonoBehaviour
+    
+    public class PolygonTarget : AbstractTarget
     {
-        public UnityEvent OnHit { get; private set; }
+        public override UnityEvent OnHit { get; protected set; }
         private Rigidbody _rigidbody;
         private Animator _targetAnimator;
+        [SerializeField] private AudioClip _hitSound;
+        private AudioSource _audioSource;
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
@@ -17,18 +18,34 @@ namespace Shooting_range
 
             _targetAnimator = GetComponentInParent<Animator>();
 
+            if (_hitSound is not null)
+                _audioSource = gameObject.AddComponent<AudioSource>();
+            
             OnHit = new UnityEvent();
             OnHit.AddListener(GetHit);
+            OnHit.AddListener(PlayHitSound);
         }
 
-        public void GetHit()
+        public override void GetHit()
         {
             _targetAnimator.Play("TargetHitted");
             Debug.Log("I got hit!");
         }
+
+        private void PlayHitSound()
+        {
+            _audioSource.PlayOneShot(_hitSound);
+        }
+
+        public override void Recover()
+        {
+            _targetAnimator.Play("TargetRecover");
+            Debug.Log("I got recover!");
+        }
         
         private void OnCollisionEnter(Collision other)
         {
+            // TODO: Прикрутить нормальный bullet
             if (!other.gameObject.TryGetComponent<Bullet>(out var bullet)) return;
             Debug.Log("Collided");
             OnHit?.Invoke();
